6a46f28ea5d263115d1ff2e8eeb97960ffe1be57
John Costello <jcostello93@gmail.com>
2025-04-10 18:01:37 +0200

    bocadillo

--- first/actions/actions.py
+++ first/actions/actions.py
@@ -1,27 +1,35 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionRecomendarPlato(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_recomendar_plato"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        print('RUNNING')
+        ingredientes = tracker.get_slot("ingrediente") or []
+        ingredientes = [i.lower() for i in ingredientes]
+
+        tiene_tomate = "tomate" in ingredientes
+        tiene_queso = "queso" in ingredientes
+        tiene_pan = "pan" in ingredientes
+
+        if tiene_tomate and tiene_queso and tiene_pan:
+            recomendacion = "Puedes preparar un bocadillo."
+        elif tiene_queso and tiene_pan:
+            recomendacion = "Puedes preparar un montadito."
+        elif tiene_tomate and tiene_pan:
+            recomendacion = "Puedes preparar una tostada."
+        elif tiene_tomate and tiene_queso:
+            recomendacion = "Puedes preparar una ensalada capresa."
+        else:
+            recomendacion = "No tienes suficientes ingredientes, te recomiendo ir a un restaurante."
+
+        dispatcher.utter_message(text=recomendacion)
+        return []
